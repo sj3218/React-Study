0feca4bb0b3946883fc02646d9d8c3668c236b89
sj3218 <phw3218@gmail.com>
2026-04-21 17:58:16 +0900

    add comment

--- pos-project/src/components/common/calendar/CommonCalendar.tsx
+++ pos-project/src/components/common/calendar/CommonCalendar.tsx
@@ -10,7 +10,7 @@ export const CommonCalendar = () => {
 
 	//1. 다른 곳 클릭하면 팝업 사라지도록 수정
 	//2. 날짜 왼쪽 값이 더 미래면, 알람 뜨도록 수정
-
+	//3. csv 다운로드 부분 제목쪽 색깔 변경해서 font도
 	return (
 		<div className="flex items-center px-5 py-2.5 bg-white border border-slate-200 rounded-full shadeow-sm">
 			<Calendar />
