57d73bef1ef1a4680f32ce7aacc3e422dfe65b3d
sj3218 <phw3218@gmail.com>
2026-04-29 17:53:00 +0900

    update detail page

--- tsx-test-project/src/pages/hyrulePages/CreatureDetailPage.tsx
+++ tsx-test-project/src/pages/hyrulePages/CreatureDetailPage.tsx
@@ -20,7 +20,51 @@ export const CreatureDetailPage = () =>{
 
     return(
         <div className="px-10 py-8 flex-1 bg-[#0F172A] text-white">
-
+            
+            {/* category */}
+            <div></div>
+            {/* image */}
+            <div></div>
+            {/* description */}
+            <div></div>
+            {/* common_location */}
+            <div></div>
+            <div key={creature.id} className="grid grid-cols-2 bg-[#12161D] border rounded-2xl overflow-hidden border-[#00C3FF]/50 transition-all shadow-[0_8px_30px_rgb(0,0,0,0.4)]">
+                <div className="aspect-square bg-[#0A0C10] relative flex items-center justify-center p-8">
+                    <div className="w-full h-full border border-[#1E2530] rounded-lg flex items-center justify-center bg-[#0d1117] group-hover:scale-105 transition-transform">
+                        <span className="text-4xl  transition-all">
+                            <img src={creature.image} alt={creature.name}/>
+                        </span>
+                    </div>
+                </div>
+                <div className="p-5 space-y-3">
+                  <div className="flex justify-between items-start">
+                    <h3 className="text-lg font-bold">{creature.name.toUpperCase()}</h3>
+                    <button className="text-gray-600">
+                        {/* <Info size={18} /> */}
+                    </button>
+                  </div>
+                  <p className="text-xs text-gray-500 line-clamp-2 leading-relaxed">
+                    {creature.description}
+                  </p>
+                  <div className="pt-3 border-t border-[#1E2530] flex items-center text-[11px] text-gray-400">
+                    {/* <Map size={12} className="mr-2" /> */}
+                    <span className="truncate">
+                        주요 서식지: {creature.common_locations?.join(', ') || '알 수 없음'}
+                    </span>
+                    <span className="truncate">
+                        Drops: {creature.drops.join(', ')}
+                    </span>
+                  </div>
+                  <div className="pt-3 border-t border-[#1E2530] flex items-center text-[11px] text-gray-400">
+                    {/* <Map size={12} className="mr-2" /> */}
+                    <span className="truncate">
+                        Drops: {creature.drops.join(', ')}
+                    </span>
+                  </div>
+                </div>
+            </div>
         </div>
     )
 }
+
